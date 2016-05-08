4a537275f4a64b4465c2130d86a2b679b20b19e7
Muhammad Faizan <muhammad.faizan01@live.com>
2016-05-08 17:02:18 +0500

    first commit

--- config/config.js
+++ config/config.js
@@ -1,3 +1,60 @@
-/**
+ /**
  * Created by Muhammad Faizan on 07-May-16.
  */
+
+ "use strict";
+ const convict = require('convict');
+
+ let config = convict({
+     env: {
+         doc: "The applicaton environment.",
+         format: ["production", "development", "test"],
+         default: "development",
+         env: "NODE_ENV"
+     },
+     server: {
+         ip: {
+             doc: "IP address to bind",
+             format: 'ipaddress',
+             default: '0.0.0.0'
+         },
+         port: {
+             doc: "port to bind",
+             format: 'port',
+             default: 8080
+         }
+     },
+     database: {
+         host: {
+             doc: "Database host name/IP",
+             format: String,
+             default: 'testing'
+         },
+         name: {
+             doc: "Database name",
+             format: String,
+             default: 'users'
+         }
+     },
+     "tokbox": {
+         "API_KEY": {
+             doc: "API key given by tokbox",
+             format: String,
+             default: "45585302"// "INSERT YOUR TOKBOX API KEY"
+         },
+         "API_SECRET": {
+             doc: "API secret given by tokbox",
+             format: String,
+             default: "a2ccb22c167cb211d733f97049acc489ccfbcda8" //"INSERT YOUR TOKBOX API SECRET"
+         }
+     }
+ });
+
+ // Load environment dependent configuration
+ var env = config.get('env');
+ config.loadFile('./config/' + env + '.json');
+
+ // Perform validation
+ config.validate({strict: false});
+
+ module.exports = config;
---
+++ index.js
@@ -0,0 +1,48 @@
+"use strict";
+
+// Dependencies
+let express = require('express'),
+  OpenTok = require('opentok'),
+  config = require('./config/config');
+
+// Verify that the API Key and API Secret are defined
+let apiKey = config.get("tokbox.API_KEY"),
+    apiSecret = config.get("tokbox.API_SECRET");
+if (!apiKey || !apiSecret) {
+    console.log('You must specify API_KEY and API_SECRET environment variables');
+    process.exit(1);
+}
+
+// Initialize the express app
+let app = express();
+app.use(express.static(__dirname + '/public'));
+
+// Initialize OpenTok
+var opentok = new OpenTok(apiKey, apiSecret);
+
+// Create a session and store it in the express app
+opentok.createSession(function(err, session) {
+    if (err) throw err;
+    app.set('sessionId', session.sessionId);
+    // We will wait on starting the app until this is done
+    init();
+});
+
+app.get('/', function(req, res) {
+    var sessionId = app.get('sessionId'),
+    // generate a fresh token for this client
+        token = opentok.generateToken(sessionId);
+
+    res.render('index.ejs', {
+        apiKey: apiKey,
+        sessionId: sessionId,
+        token: token
+    });
+});
+
+// Start the express app
+function init() {
+    app.listen(3000, function() {
+        console.log('You\'re app is now ready at http://localhost:3000/');
+    });
+}
